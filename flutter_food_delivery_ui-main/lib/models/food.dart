c5d98f0697a92ea9a9d522b4ab9ecd5de38df9e5
JeongInPark <nickcaestro@gmail.com>
2021-09-11 17:39:06 +0900

    Flutter Food Deliver Ui Studying

--- flutter_food_delivery_ui-main/lib/models/food.dart
+++ flutter_food_delivery_ui-main/lib/models/food.dart
@@ -1,3 +1,5 @@
+
+
 class Food {
   final String imageUrl;
   final String name;
---
+++ flutter_food_delivery_ui-main/lib/screens/restaurant_screen.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+
+class RestaurantScreen extends StatefulWidget {
+   final Restaurant restaurant;
+
+   RestaurantScreen({this.restaurant});
+
+  @override
+  _RestaurantScreenState createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
