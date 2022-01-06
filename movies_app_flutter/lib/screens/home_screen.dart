ef51aa362e51a5c1784ace7c8080e22db65ff532
JeongInPark <nickcaestro@gmail.com>
2022-01-06 17:18:22 +0900

    test

--- movies_app_flutter/lib/screens/home_screen.dart
+++
@@ -1,6 +0,0 @@
-// TODO Implement this library.
-
-
-
-
-
--- movies_app_flutter/lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:movies_app_flutter/screens/home_screen.dart';
-import 'package:sizer/sizer.dart';
-
-final GlobalKey<HomeScreenState> kHomeScreenKey = GlobalKey();
-
-
-
-const kPrimaryColor = Color(0xFF101010);
