6485c632fec649bdf457f5f140be343f51a30cb0
JeongInPark <nickcaestro@gmail.com>
2022-06-07 20:26:57 +0900

    카카오 로그인

---
+++ kakao_login_01/android/app/src/main/kotlin/com/nickbreeze/kakao_login_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nickbreeze.kakao_login_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kakao_login_01/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:kakao_login_01/kakao_login.dart';
+
+import 'main_view_model.dart';
+
+void main() {
+  KakaoSdk.init(nativeAppKey: '86c179189a2511253423a27b51eccf69');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final viewModel = MainViewModel(KakaoLogin());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+                viewModel.user?.kakaoAccount?.profile?.profileImageUrl ?? ''),
+            Text(
+              '${viewModel.isLogined}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await viewModel.login();
+                setState(() {});
+              },
+              child: const Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await viewModel.logout();
+                setState(() {});
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
