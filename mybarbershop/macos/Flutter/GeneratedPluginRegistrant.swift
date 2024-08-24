d4f21624163ba409a869a2167ab82fcb513a364b
JeongInPark <nickbreeze7@gmail.com>
2024-08-24 19:10:06 +0900

    소스정리

--- mybarbershop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mybarbershop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,15 +8,19 @@ import Foundation
 import firebase_auth
 import firebase_core
 import geolocator_apple
+import google_sign_in_ios
 import location
 import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
